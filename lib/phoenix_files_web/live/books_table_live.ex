b68dc08e844b6a011a5a79d1b37139608fa7621f
Berenice Medel <bemesa21@gmail.com>
2022-05-03 16:02:39 -0500

    refactor
    - Set the MultiSelectComponent as a part of a parent form instead of using it as an independent form
    - Add `selected` and `form` assigns to MultiSelectComponent
    - `form` param is used inside the `MultiSelectComponent` to use the `inputs_for` function
    - `selected` function is executed when there are updates in the selected_options inside MultiSelectComponent, in this case is used to send updates to the parent LiveView
    - Handle recovery when the connection is lost by adding phx-change="validate" to the form

--- lib/phoenix_files_web/live/books_table_live.ex
+++ lib/phoenix_files_web/live/books_table_live.ex
@@ -1,6 +1,7 @@
 defmodule PhoenixFilesWeb.BooksTableLive do
   use PhoenixFilesWeb, :live_view
 
+  alias PhoenixFiles.MultiSelect
   alias PhoenixFiles.MultiSelect.SelectOption
   alias PhoenixFiles.Book
 
@@ -11,41 +12,42 @@ defmodule PhoenixFilesWeb.BooksTableLive do
         %{id: 2, label: "Horror", selected: true},
         %{id: 3, label: "Literary Fiction", selected: false}
       ]
-
       # hardcoded values
       |> build_options()
 
-    books = filter_books(options)
 
-    socket =
-      socket
-      |> assign(:options, options)
-      |> assign(:books, books)
-
-    {:ok, socket}
+    {:ok, assign_multi_select_options(socket, options)}
   end
 
   def render(assigns) do
     ~H"""
     <div class="relative">
-      <.live_component id="multi" module={PhoenixFilesWeb.MultiSelectComponent} options={@options} />
+      <.form let={f} for={@changeset}  id="multiselect-form" phx-change="validate">
+        <.live_component
+          id="multi"
+          module={PhoenixFilesWeb.MultiSelectComponent}
+          options={@options}
+          form={f}
+          selected={fn opts -> send(self(), {:updated_options, opts}) end}
+        />
+      </.form>
     </div>
     <div class="grid grid-cols-5">
       <%= for book <- @books do %>
-      <div class="max-w-sm rounded overflow-hidden shadow-lg m-5">
-        <img class="w-full h-96 object-contain" src={book.cover} alt="Sunset in the mountains">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2"><%= book.title %></div>
-            <p class="text-gray-700 text-base">
-              <%= book.description %>
-            </p>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg m-5">
+          <img class="w-full h-96 object-contain" src={book.cover} alt="Sunset in the mountains">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2"><%= book.title %></div>
+              <p class="text-gray-700 text-base">
+                <%= book.description %>
+              </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <%= for category <- book.category do %>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#<%= category %></span>
+            <% end %>
+          </div>
         </div>
-        <div class="px-6 pt-4 pb-2">
-          <%= for category <- book.category do %>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#<%= category %></span>
-          <% end %>
-        </div>
-      </div>
       <% end %>
     </div>
     """
@@ -54,20 +56,33 @@ defmodule PhoenixFilesWeb.BooksTableLive do
   def handle_info({:updated_options, options}, socket) do
     # update the list of options in the multiselect component
 
-    books = filter_books(options)
+    {:noreply, assign_multi_select_options(socket, options)}
+  end
 
-    socket =
-      socket
-      |> assign(:books, books)
-      |> assign(:options, options)
+  def handle_event("validate", %{"multi_select" => multi_component}, socket) do
+    options = build_options(multi_component["options"])
 
-    {:noreply, socket}
+    {:noreply, assign_multi_select_options(socket, options)}
+  end
+
+  defp assign_multi_select_options(socket, options) do
+    socket
+    |> assign(:changeset, build_changeset(options))
+    |> assign(:books, filter_books(options))
+    |> assign(:options, options)
   end
 
   defp build_options(options) do
-    Enum.reduce(options, [], fn data, acc ->
-      [%SelectOption{id: data.id, label: data.label, selected: data.selected} | acc]
+    Enum.map(options, fn
+      {_idx, data} -> %SelectOption{id: data["id"], label: data["label"], selected: data["selected"]}
+      data -> %SelectOption{id: data.id, label: data.label, selected: data.selected}
     end)
+  end
+
+  defp build_changeset(options) do
+    %MultiSelect{}
+    |> Ecto.Changeset.change()
+    |> Ecto.Changeset.put_embed(:options, options)
   end
 
   defp filter_books(options) do
--- lib/phoenix_files_web/live/multi_select_component.ex
+++ lib/phoenix_files_web/live/multi_select_component.ex
@@ -1,8 +1,6 @@
 defmodule PhoenixFilesWeb.MultiSelectComponent do
   use PhoenixFilesWeb, :live_component
 
-  alias PhoenixFiles.MultiSelect
-
   def render(assigns) do
     ~H"""
     <div class="flex justify-center">
@@ -13,35 +11,36 @@ defmodule PhoenixFilesWeb.MultiSelectComponent do
           </div>
         <% end %>
         <div class="absolute right-0">
-          <svg id="down" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 absolute right-0 top-3" viewBox="0 0 20 20" fill="currentColor" phx-click={JS.toggle() |> JS.toggle(to: "#up") |> JS.toggle(to: "#multiselect-form")}>
+          <svg id="down" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 absolute right-0 top-3" viewBox="0 0 20 20" fill="currentColor" phx-click={JS.toggle() |> JS.toggle(to: "#up") |> JS.toggle(to: "#options-container")}>
             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
           </svg>
-          <svg id="up" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5  absolute right-0 top-3 hidden" viewBox="0 0 20 20" fill="currentColor"  phx-click={JS.toggle() |> JS.toggle(to: "#down") |> JS.toggle(to: "#multiselect-form")}>
+          <svg id="up" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5  absolute right-0 top-3 hidden" viewBox="0 0 20 20" fill="currentColor"  phx-click={JS.toggle() |> JS.toggle(to: "#down") |> JS.toggle(to: "#options-container")}>
             <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
           </svg>
          </div>
       </div>
-      <.form let={f} for={@changeset} phx-target={@myself} class="hidden w-96 mt-4 p-4 ml-2 mt-16 absolute z-10 bg-stone-50 shadow-2xl rounded-lg" id="multiselect-form">
-        <%= inputs_for f, :options, fn value -> %>
+      <div class="hidden w-96 mt-4 p-4 ml-2 mt-16 absolute z-10 bg-stone-50 shadow-2xl rounded-lg" id="options-container">
+        <%= inputs_for @form, :options, fn value -> %>
           <div class="form-check">
             <label class="form-check-label inline-block text-gray-800">
-              <%= checkbox value, :selected, phx_change: "checked", value: value.data.selected, class: "form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" %>
-              <span class="ml-2"><%= value.data.label %></span>
+              <%= checkbox value, :selected, phx_change: "checked",  phx_target: @myself, value: value.data.selected, class: "form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" %>
+              <%= label value, :label, value.data.label, class: "ml-2" %>
+              <%= hidden_input value, :label, value: value.data.label %>
             </label>
           </div>
         <% end %>
-      </.form>
+      </div>
     </div>
     """
   end
-
-  def update(%{options: options}, socket) do
-    changeset = build_changeset(options)
-
+  def update(params, socket) do
+    %{options: options, form: form, selected: selected} = params
     socket =
       socket
-      |> assign(:changeset, changeset)
       |> assign(:selected_options, filter_selected_options(options))
+      |> assign(:options, options)
+      |> assign(:form, form)
+      |> assign(:selected, selected)
 
     {:ok, socket}
   end
@@ -49,20 +48,14 @@ defmodule PhoenixFilesWeb.MultiSelectComponent do
   def handle_event("checked", %{"multi_select" => %{"options" => values}}, socket) do
     [{index, %{"selected" => selected?}}] = Map.to_list(values)
     index = String.to_integer(index)
-    multi_options = Ecto.Changeset.get_field(socket.assigns.changeset, :options)
-    current_option = Enum.at(multi_options, index)
+    current_option = Enum.at(socket.assigns.options, index)
 
     updated_options =
-      List.replace_at(multi_options, index, %{current_option | selected: selected?})
+      List.replace_at(socket.assigns.options, index, %{current_option | selected: selected?})
 
-    send(self(), {:updated_options, updated_options})
+    socket.assigns.selected.(updated_options)
+
     {:noreply, socket}
-  end
-
-  defp build_changeset(options) do
-    %MultiSelect{}
-    |> Ecto.Changeset.change()
-    |> Ecto.Changeset.put_embed(:options, options)
   end
 
   defp filter_selected_options(options) do
