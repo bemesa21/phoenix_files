9a578f8baa58371412615784918c8bf4f5fdeb72
Berenice Medel <bemesa21@gmail.com>
2022-06-03 15:50:35 -0500

    add spinner and phx:js-exec listener

---
+++ lib/phoenix_files_web/live/live_helpers.ex
@@ -0,0 +1,35 @@
+defmodule PhoenixFilesWeb.LiveHelpers do
+  import Phoenix.LiveView
+  import Phoenix.LiveView.Helpers
+
+  alias Phoenix.LiveView.JS
+
+
+  def loader(assigns) do
+    assigns
+    |> assign(assigns_to_attributes(assigns, [:id, :class]))
+
+    ~H"""
+    <div class="hidden h-full bg-slate-100" id={@id} data-show={show_loader(@id)} data-hide={hide_loader(@id)}>
+      <div class="flex justify-center items-center h-full">
+        <div class="flower-spinner">
+          <div class="dots-container">
+            <div class="bigger-dot">
+              <div class="smaller-dot"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  defp show_loader(js \\ %JS{}, id) do
+    JS.show(js, to: "##{id}", transition: {"ease-in duration-300", "opacity-0", "opacity-100"})
+  end
+
+  defp hide_loader(js \\ %JS{}, id) do
+    JS.hide(js, to: "##{id}", transition: {"ease-out duration-300", "opacity-100", "opacity-0"})
+  end
+
+end
